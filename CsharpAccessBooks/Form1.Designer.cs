4785489f8d5825bac3ffff9f6c9cece76da766a6
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2017-05-24 21:33:42 -0700

    Add files via upload

---
+++ CsharpAccessBooks/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace CsharpAccessBooks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.rdbNo = new System.Windows.Forms.RadioButton();
+            this.rdbYes = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ISBN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Author";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Category";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ebook ";
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(129, 22);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(258, 20);
+            this.txtISBN.TabIndex = 5;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(129, 61);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(258, 20);
+            this.txtTitle.TabIndex = 6;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(129, 102);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(258, 20);
+            this.txtAuthor.TabIndex = 7;
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(129, 144);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(258, 20);
+            this.txtCategory.TabIndex = 8;
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(135, 242);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(93, 32);
+            this.btnLast.TabIndex = 11;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(36, 280);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(93, 32);
+            this.btnNext.TabIndex = 12;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(135, 280);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(93, 32);
+            this.btnPrevious.TabIndex = 13;
+            this.btnPrevious.Text = "Previous ";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(248, 242);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(93, 32);
+            this.btnSearch.TabIndex = 15;
+            this.btnSearch.Text = "Search ISBN";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(347, 242);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(93, 32);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "Add New";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(248, 280);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(93, 32);
+            this.btnModify.TabIndex = 17;
+            this.btnModify.Text = "Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(347, 280);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(93, 32);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(36, 242);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(95, 32);
+            this.btnFirst.TabIndex = 21;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(36, 360);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(115, 31);
+            this.btnClear.TabIndex = 22;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // rdbNo
+            // 
+            this.rdbNo.AutoSize = true;
+            this.rdbNo.Location = new System.Drawing.Point(237, 187);
+            this.rdbNo.Name = "rdbNo";
+            this.rdbNo.Size = new System.Drawing.Size(39, 17);
+            this.rdbNo.TabIndex = 24;
+            this.rdbNo.TabStop = true;
+            this.rdbNo.Text = "No";
+            this.rdbNo.UseVisualStyleBackColor = true;
+            // 
+            // rdbYes
+            // 
+            this.rdbYes.AutoSize = true;
+            this.rdbYes.Location = new System.Drawing.Point(150, 187);
+            this.rdbYes.Name = "rdbYes";
+            this.rdbYes.Size = new System.Drawing.Size(43, 17);
+            this.rdbYes.TabIndex = 23;
+            this.rdbYes.TabStop = true;
+            this.rdbYes.Text = "Yes";
+            this.rdbYes.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 419);
+            this.Controls.Add(this.rdbNo);
+            this.Controls.Add(this.rdbYes);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RadioButton rdbNo;
+        private System.Windows.Forms.RadioButton rdbYes;
+    }
+}
+
---
+++ CsharpAccessBooks/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpAccessBooks
+{
+    public partial class Form1 : Form
+    {
+        ADODB.Connection Con;
+        ADODB.Recordset Rs;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Con = new ADODB.Connection();
+            Rs = new ADODB.Recordset();
+            Con.Provider = "Microsoft.jet.oledb.4.0";
+            Con.ConnectionString = "G:\\Assignment 3  Access CSharp\\CsharpAccessBooks\\Books.mdb";
+            Con.Open();
+            Rs.Open("Select * from BooksTable", Con, ADODB.CursorTypeEnum.adOpenDynamic, ADODB.LockTypeEnum.adLockOptimistic);
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            if (Rs.EOF == true && Rs.BOF == true)
+            {
+                MessageBox.Show("Table is empty!");
+                return;
+            }
+            Rs.MoveFirst();
+            ShowDataOnForm();
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            Rs.MoveLast();
+            if (Rs.EOF == true)
+            {
+                MessageBox.Show("End of the table.");
+                return;
+            }
+            ShowDataOnForm();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (Rs.EOF == true && Rs.BOF == true)
+            {
+                MessageBox.Show("Table is empty!");
+                return;
+            }
+            Rs.MoveNext();
+            if(Rs.EOF == true )
+            {
+                Rs.MovePrevious(); 
+                MessageBox.Show("Passed end of file.");
+            }
+            ShowDataOnForm(); 
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (Rs.EOF == true && Rs.BOF == true)
+            {
+                MessageBox.Show("Table is empty!");
+                return;
+            }
+            Rs.MovePrevious();
+            if(Rs.BOF == true )
+            {
+                Rs.MoveNext(); 
+                MessageBox.Show("Passed beginning of file.");
+            }
+            ShowDataOnForm(); 
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearBoxes();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string FindRecord;
+
+            if (txtISBN.Text == "")
+            {
+                MessageBox.Show("Please provide ID to Search");
+                return;
+            }
+            else
+            {
+                FindRecord = "ISBN = " + txtISBN.Text;
+                Rs.MoveFirst();
+                Rs.Find(FindRecord);
+                if (Rs.EOF == true)
+                {
+                    // Record has not been found 
+                    MessageBox.Show("Record not found!");
+                    return;
+                }
+                else
+                {
+                    ShowDataOnForm();
+                    return;
+                }
+
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string FindRecord;
+
+            if (txtISBN.Text == "" && txtTitle.Text == "" && txtAuthor.Text == "" && txtCategory.Text == "" )
+            {
+                MessageBox.Show("Please fill up all the fields.");
+                return;
+            }
+            else if (rdbYes.Checked == false && rdbNo.Checked == false)
+            {
+                MessageBox.Show("Please select Yes or No in Ebook.");
+                return;
+            }
+            else
+            {
+                FindRecord = "ISBN = " + txtISBN.Text;
+                Rs.MoveFirst();
+                Rs.Find(FindRecord);
+                if (Rs.EOF == true)
+                {
+                    // Record has not been found, so we can add it.
+                    Rs.AddNew();
+                    SaveInDataBase();
+                    Rs.Update();
+                    MessageBox.Show("Record saved succesfully!");
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Duplicated ID, use another one.");
+                    ShowDataOnForm();
+                    return;
+                }
+
+            }
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            string FindRecord;
+
+            if (txtISBN.Text == "")
+            {
+                MessageBox.Show("Please provide ID to search and modify.");
+                return;
+            }
+            else
+            {
+                FindRecord = "ISBN = " + txtISBN.Text;
+                Rs.MoveFirst();
+                Rs.Find(FindRecord);
+                if (Rs.EOF == true)
+                {
+                    // Record has not been found. 
+                    MessageBox.Show("Record not found, nothing can be modified.");
+                    return;
+                }
+                else
+                {
+                    SaveInDataBase();
+                    Rs.Update();
+                    MessageBox.Show("Record modified succesfully!");
+                    return;
+                }
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string FindRecord;
+            DialogResult Selection;
+
+            if (txtISBN.Text == "")
+            {
+                MessageBox.Show("Please provide ID to search and delete.");
+                return;
+            }
+            else
+            {
+                FindRecord = "ISBN = " + txtISBN.Text;
+                Rs.MoveFirst();
+                Rs.Find(FindRecord);
+                if (Rs.EOF)
+                {
+                    // Record has not been found 
+                    MessageBox.Show("Record not found, nothing can be deleted.");
+                    return;
+                }
+                else
+                {
+                    Selection = MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo);
+                    if (Selection == DialogResult.Yes)
+                    {
+                        Rs.Delete();
+                        ClearBoxes();
+                        MessageBox.Show("Record deleted succesfully!");
+                        Rs.Update();
+
+                    }
+
+                    return;
+                }
+
+            }
+        }
+
+#region "Functions"
+
+        public void SaveInDataBase()
+        {
+            Rs.Fields["ISBN"].Value = Convert.ToInt32(txtISBN.Text);
+            Rs.Fields["Title"].Value = txtTitle.Text;
+            Rs.Fields["Author"].Value = txtAuthor.Text;
+            Rs.Fields["Category"].Value = txtCategory.Text;
+            if (rdbYes.Checked)
+            {
+                Rs.Fields["ebook"].Value = true;
+            }
+            else
+            {
+                Rs.Fields["ebook"].Value = false;
+
+            }
+        }
+
+        public void ShowDataOnForm()
+        {
+            txtISBN.Text = Convert.ToString(Rs.Fields["ISBN"].Value);
+            txtTitle.Text = Convert.ToString(Rs.Fields["Title"].Value);
+            txtAuthor.Text = Convert.ToString(Rs.Fields["Author"].Value);
+            txtCategory.Text = Convert.ToString(Rs.Fields["Category"].Value);
+            if (Rs.Fields["ebook"].Value == true)
+            {
+                rdbYes.Checked = true;
+                rdbNo.Checked = false;
+            }
+            else
+            {
+                rdbYes.Checked = false;
+                rdbNo.Checked = true;
+            }
+           
+        }
+
+        public void ClearBoxes()
+        {
+            txtISBN.Clear();
+            txtTitle.Clear();
+            txtAuthor.Clear();
+            txtCategory.Clear();
+            rdbYes.Checked = false;
+            rdbNo.Checked = false;
+        }
+
+    } 
+}
+#endregion "Functions"
